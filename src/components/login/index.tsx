bf5de8a6f784739464115f477362f94cb5f7cf08
Dieter-VanderZwalmen <55390044+Dieter-VanderZwalmen@users.noreply.github.com>
2022-05-24 20:17:39 +0200

    login werkt eindelijk

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -1,45 +1,49 @@
+
+import React, { useState } from 'react';
 import { AxiosResponse } from 'axios';
-import React, { useState, useEffect } from 'react';
 import LoginService from '../../services/loginService';
-import LoginForm from './LoginForm';
-import MessagesOverviewTable from '../message-overview/MessagesOverviewTable';
+import { Message,Response,StatusMessage,User } from '../../types/indexUser';
+import classNames from 'classnames';
+import { json } from 'stream/consumers';
+import MessageOverview from '../message-overview';
 
-
-
-
-const login: React.FC = () => {
+const Login: React.FC = () => {
+    const [statusMessages, setStatusMessages] = useState<StatusMessage[]>([]);
+    const [nameInput, setNameInput] = useState<string>("");
 
     //async functie met parameter naam
     const login = async (name: string) => {
         try {
             //res == het resultaat van login => res is dus gelijk aan X of Y  (nog in te vullen)
             //laat loginservice error gooien indien het niet lukt??
-            const res = await LoginService.login("Dieter");
-
-            //key logginUser zou gelijk moeten gesteld worden aan de naam 
-            sessionStorage.setItem("loggedinUser", name)
-
-            //setStatusMessages([{message: "User is ingelogd", type: succes}])
-
-            window.location.reload();
-
-
-        } catch (error: any) {
-            //hier moeten we eigenlijk gewoon een fout melding kunnen geven
-            //setStatusMessage([...statusMessages, {message: error, type: "error"} ])
+            //login returned naam van user
+            setStatusMessages([{ message: 'trying to log in with the name.', type: 'success' }]);
             
-        }
-       //hier zou ik moeten kunnen zeggen return naar messages pagina
-       //of return deze pagina
-       //of ik maak deze functie void
-        
+            await LoginService.login('{"name": "'+name+'"}');//deze lijn zou een error moeten gooien hopelijk indien het niet lukt
 
+            setStatusMessages([{ message: 'Logging in.', type: 'success' }]);
+            sessionStorage.setItem("loggedinUser", name)
+            setStatusMessages([{message: "User is ingelogd", type: 'success'}])
+            
+
+            //rederict naar messages??
+        } catch (any) {
+            //hier moeten we eigenlijk gewoon een fout melding kunnen geven
+            setStatusMessages([...statusMessages, {message: "Naam niet gevonden", type: "error"} ])
+            setNameInput('');
+        }
     }
+
     //wat te doen als je op submit duwt
     const handleSubmit = (event: any) => {
         //overschrijf de default
         event.preventDefault()
 
+        if (nameInput.trim() === '') {
+            setStatusMessages([{ message: 'Please fill in name.', type: 'error' }]);
+        }else{
+            login(nameInput);
+        }
         //while(statusMessages.length > 0) { statusMessages.pop(); }
         
 
@@ -48,12 +52,40 @@ const login: React.FC = () => {
     }
    
     
-    return  <section className="row justify-content-center">
-         <LoginForm
-                
-            />   
-            </section>
-};
-
-export default login;
-
+    return   (
+        <section className="row justify-content-center">
+            {statusMessages && (
+                <ul className="list-unstyled col-4 mb-3">
+                    {statusMessages.map(({ message, type }, index) => (
+                        <li
+                            key={index}
+                            className={classNames({
+                                'text-danger': type === 'error',
+                                'text-success': type === 'success',
+                            })}
+                        >
+                            {message}
+                        </li>
+                    ))}
+                </ul>
+            )}
+        <div className="col-4 mb-3">
+        <form onSubmit={handleSubmit}>
+                      <label>
+                          Name:
+                          <input
+                              className="m-sm-2"
+                              type="text"
+                              value={nameInput}
+                              onChange={(event) => setNameInput(event.target.value)}
+                          />
+                      </label>
+                      <input type="submit" value="Submit" />
+                  </form>
+                  </div>
+                  </section>
+               
+     
+      );
+    };
+export default Login;
--- src/services/loginService.ts
+++ src/services/loginService.ts
@@ -1,7 +1,7 @@
 import axios from '../axios';
 import { Message, User } from '../types/indexUser';
 
-const login = (naam : string) => axios.post<JSON>('/users/login');
+const login = (naam : string) => axios.post<JSON>('/users/login', naam);
 
 const LoginService = {
     login
